854655ef4379756d0c0860f7359b9603a87f2905
abhiraftaar007 <singhabhishekas786@gmail.com>
2025-01-16 23:30:48 +0530

    Components, Props, Components in Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import "./App.css";
+// import Car from "./Car";
+import Garage from "./Garage";
+// Components are like functions that return HTML elements.
+// Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and return HTML.
+// Components come in two types, Class components and Function components
+
+/*
+Components can be passed as props, which stands for properties.
+
+Props are like function arguments, and you send them into the component as attributes. 
+*/
+
+// Components in Components - refer Garage and Car file
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      {/* <Car color="red" /> */}
+      <Garage/>
     </div>
   );
 }
---
+++ src/Garage.js
@@ -0,0 +1,11 @@
+import Car from "./Car"
+function Garage(){
+    return (
+        <>
+            <h1>Who lives in my Garage?</h1>
+            <Car color="red"/>
+        </>
+    )
+}
+
+export default Garage;
