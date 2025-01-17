796189a3fa61a31916ad0824d1bc7845d6afe7e2
abhiraftaar007 <singhabhishekas786@gmail.com>
2025-01-17 16:41:25 +0530

    React Memo to skip rendering a component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Todos from "./Todos";
 
+/* 
+Using memo will cause React to skip rendering a component if its props have not changed.
+This can improve performance.
+
+Problem - 
+When you click the increment button, the Todos component re-renders.
+If this component was complex, it could cause performance issues.
+
+Solution -
+To fix this, we can use memo.
+Use memo to keep the Todos component from needlessly re-rendering.
+Wrap the Todos component export in memo
+*/
 function App() {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Todos todos={todos} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
     </div>
   );
 }
---
+++ src/Todos.js
@@ -0,0 +1,16 @@
+import { memo } from "react";
+
+const Todos = ({ todos }) => {
+  console.log("child render");
+  return (
+    <>
+      <h2>My Todos</h2>
+      {todos.map((todo, index) => {
+        return <p key={index}>{todo}</p>;
+      })}
+    </>
+  );
+};
+
+export default memo(Todos);
+// Now the Todos component only re-renders when the todos that are passed to it through props are updated.
